643d8ff587cf12ba2490fb8f4d25ef8d62ad4c00
Finersoft <290384427@qq.com>
2018-01-29 17:30:01 +0800

    添加第一章mysql脚本

---
+++ ch0InitWorks/db_mysql_ch1.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `youhua`.`userinfo`;
+CREATE TABLE  `youhua`.`userinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
